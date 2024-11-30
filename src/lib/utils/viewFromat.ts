fe9b4dd16f088dfe6f51e649e062c95f08511b7a
EV-OD <evod599@gmail.com>
2024-11-30 16:41:34 +0545

    added video and gamming section

---
+++ src/lib/utils/viewFromat.ts
@@ -0,0 +1,9 @@
+export default function viewsFormat(views: number): string {
+  if (views < 1000) {
+    return views.toString();
+  } else if (views < 1000000) {
+    return `${(views / 1000).toFixed(1)}K`;
+  } else {
+    return `${(views / 1000000).toFixed(1)}M`;
+  }
+}
