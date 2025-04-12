8b68925e19f26512c358099de5ec1bd2b332e29a
Maddat Subedi <163100085+maddatsubedi@users.noreply.github.com>
2025-04-12 22:24:26 +0545

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -40,7 +40,7 @@ export default defineConfig({
         "@/shortcodes/Blockquote",
         "@/shortcodes/Badge",
         "@/shortcodes/ContentBlock",
-        "@/shortcodes/Journey",
+        "@/shortcodes/JourneyCard",
         "@/shortcodes/Tab",
         "@/shortcodes/Tabs",
       ],
