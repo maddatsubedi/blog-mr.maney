6755e2fe2d0985af7190968ec778d7ea8a12ab9c
Maddat Subedi <163100085+maddatsubedi@users.noreply.github.com>
2025-04-19 20:28:03 +0545

    fixed about mainlink icon mapper

--- src/layouts/function-components/LinkIconMap.jsx
+++ src/layouts/function-components/LinkIconMap.jsx
@@ -2,16 +2,18 @@ import React from 'react';
 import { FaBlog, FaDotCircle, FaUserCircle } from "react-icons/fa";
 import { MdOutlineContactMail } from "react-icons/md";
 
+const DEFULT_ICON = <FaDotCircle />;
+
 const map = {
-    blog: <FaBlog />,
-    about: <FaUserCircle />,
-    contact: <MdOutlineContactMail />
+    "/blog": <FaBlog />,
+    "/about": <FaUserCircle />,
+    "/contact": <MdOutlineContactMail />
 }
 
 const LinkIconMap = ({ linkTo }) => {
     return (
         <>
-            {map[linkTo] || <FaDotCircle />}
+            {map[linkTo] || DEFULT_ICON}
         </>
     )
 }
